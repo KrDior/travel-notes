a282b29d59918a57282e0ec6d125e3b398c1a55f
Saveli Savich <Saveli_Savich@epam.com>
2019-09-27 16:31:16 +0300

    add test comps

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import * as React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+import Header from "./component/testStateless/Header";
+import Description from "./component/testStateComponent/Description";
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Header name="PicaPica" />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,8 +23,9 @@ const App: React.FC = () => {
           Learn React
         </a>
       </header>
+      <Description countBy={3} />
     </div>
   );
-}
+};
 
 export default App;
